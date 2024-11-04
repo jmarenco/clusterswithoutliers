755742bb91f5c692de61911eed95aa1ca3d045f7
Borelian <eduardo.moreno@gmail.com>
2024-11-04 13:33:07 +0100

    Added code to make a solution feasible for all points by extending its borders

---
+++ Clustering/src/incremental/FeasibleSolutionHeuristic.java
@@ -0,0 +1,110 @@
+package incremental;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import general.Instance;
+import general.Point;
+import general.RectangularCluster;
+
+public class FeasibleSolutionHeuristic {
+	
+	private Instance _ins;
+	private double[] _minDistanceToCluster;
+	private int[] _closest_cluster;
+
+	private int _ndim;
+	private int _npoints;
+	
+	public FeasibleSolutionHeuristic(Instance ins) 
+	{
+		_ins = ins;
+		_ndim = _ins.getDimension();
+		_npoints = ins.getPoints();
+	}
+	
+	
+	public ArrayList<RectangularCluster> MakeFeasible(ArrayList<RectangularCluster> original_clusters)
+	{
+		ArrayList<RectangularCluster> clusters = original_clusters;
+		// Create a priority queue w.r.t minimum distance to clusters.
+		ComputeDistanceToClusters(clusters);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(_npoints, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Double.compare(_minDistanceToCluster[b],_minDistanceToCluster[a]);
+            }
+        });
+		Set<Integer> in_queue = new HashSet<Integer>();
+		for (int i=0; i< _npoints; i++) {
+			if (_minDistanceToCluster[i] > 0) {
+				pq.offer(i);
+				in_queue.add(i);
+			}
+        }
+		// Main loop until all points are covered.
+		while (!pq.isEmpty()) {
+			int point_to_include = pq.poll();
+			int closest_cluster = _closest_cluster[point_to_include];
+			Point point = _ins.getPoint(point_to_include);
+			
+			// Change limits of the cluster;
+			for (int t=0; t<_ndim; t++)
+			{
+				if (point.get(t) > clusters.get(closest_cluster).getMax(t)) {
+					clusters.get(closest_cluster).setMax(t, point.get(t));
+				} else if (point.get(t) < clusters.get(closest_cluster).getMin(t)) {
+					clusters.get(closest_cluster).setMin(t, point.get(t));
+				}
+			}
+			_closest_cluster[point_to_include] = closest_cluster;
+			_minDistanceToCluster[point_to_include] = 0.0;
+			clusters.get(closest_cluster).add(_ins.getPoint(point_to_include));
+			// Update distance of remaining points.
+			Iterator<Integer> iterator = in_queue.iterator();
+			while (iterator.hasNext()) {
+			    Integer p = iterator.next();
+			    double newClusterDistance = DistanceToCluster(_ins.getPoint(p), clusters.get(closest_cluster));
+			    if (_minDistanceToCluster[p] > newClusterDistance) {
+			    	_closest_cluster[p] = closest_cluster;
+			    	_minDistanceToCluster[p] = newClusterDistance;
+			    	pq.remove(p);
+			    	if (newClusterDistance > 0) {
+			    		pq.offer(p);
+			    	} else {
+						clusters.get(closest_cluster).add(_ins.getPoint(p));
+			    		iterator.remove();
+			    	}
+			    }
+			}
+		}
+		return clusters;
+	}
+	
+	private double DistanceToCluster(Point p, RectangularCluster c) {
+		double distance = 0.0;
+		for (int t=0; t<_ndim; t++)
+		{
+			distance += Math.max(p.get(t) - c.getMax(t), 0.0);
+			distance += Math.max(c.getMin(t) - p.get(t), 0.0);
+		}
+		return distance;
+	}
+	
+	private void ComputeDistanceToClusters(ArrayList<RectangularCluster> clusters) {
+		for (int i=0; i< _npoints; i++) {
+			_minDistanceToCluster[i] = DistanceToCluster(_ins.getPoint(i), clusters.get(0));
+			_closest_cluster[i] = 0;
+			for (int j=1; j<clusters.size(); j++) {
+				double distance = DistanceToCluster(_ins.getPoint(i), clusters.get(j));
+				if (distance < _minDistanceToCluster[i]) {
+					_minDistanceToCluster[i] = distance;
+					_closest_cluster[i] = j;
+				}
+			}
+		}
+	}
+}
